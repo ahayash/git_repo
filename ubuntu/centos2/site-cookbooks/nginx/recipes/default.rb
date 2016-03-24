cd194b909f0894df72b3c1d7a9a7f830dcc41cb7
ahayashi <hayashi@proud.co.jp>
2016-03-24 17:07:41 +0900

    PHP provisionedgit add centos2 to centos2.

--- ubuntu/centos2/site-cookbooks/nginx/recipes/default.rb
+++ ubuntu/centos2/site-cookbooks/nginx/recipes/default.rb
@@ -19,3 +19,13 @@ service "nginx" do
   supports :status => true, :restart => true, :reload => true
 end
 
+
+template 'nginx.conf' do
+  path '/etc/nginx/nginx.conf'
+  source "nginx.conf.erb"
+  owner 'root'
+  group 'root'
+  mode '0644'
+  notifies :reload, "service[nginx]"
+end
+
